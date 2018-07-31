dd14ad9d3a6435e1d97c0815c4ba6942265f363a
Richard Memory <richardfmemory@gmail.com>
2018-07-30 19:09:31 -0600

    Added async/await

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,11 +5,23 @@ import axios from 'axios';
 class App extends Component {
 	state = {
 		posts: [],
+		err: false,
+		errMsg: '',
 	}
 
-	componentDidMount() {
-		axios.get('https://www.reddit.com/r/reactjs.json')
-			.then((res) => {
+	async componentDidMount() {
+		try {
+			/* When the promise is fulfulled, state will be updated and the
+			   component updated. We could use a simple
+
+				get().then((res) => { blah blah }
+
+				but I'll use async and await instead, which allows me to
+				display a simple loading message while waiting for the data */
+			const result = await axios
+				.get('https://www.reddit.com/r/reactjs.json');
+
+			if (result) {
 				/* Drill into the response to extract the titles
 				   of the various posts which are found under
 				   res.data.data.children, which is a list of objects
@@ -19,22 +31,53 @@ class App extends Component {
 				   returns an list containing the all of the obj.data's.
 				   These are cast by the application below to be a "post",
 				   from which we obtain the id and title. */
-				const posts = res.data.data.children.map(obj => obj.data);
+
+				const posts = result.data.data.children.map(obj => obj.data);
 				this.setState({ posts });
+			} else {
+				this.setState({ // eslint-disable-line react/no-did-mount-set-state
+					posts: [],
+					err: true,
+					errMsg: 'Invalid movie data',
+				});
+			}
+		} catch (e) {
+			this.setState({ // eslint-disable-line react/no-did-mount-set-state
+				posts: [],
+				err: true,
+				errMsg: `Invalid movie data: ${e.name}: ${e.message}`,
 			});
+		}
 	}
 
 	render() {
+		const { err, errMsg, posts } = this.state;
+		let message;
+
+		if (!err) {
+			message = 'Loading posts ...';
+		} else {
+			message = errMsg;
+		}
+
 		return (
 			<div>
 				<h1>
 					/r/react.js
 				</h1>
-				<ul>
-					{this.state.posts.map(post => ( // eslint-disable-line react/destructuring-assignment
-						<li key={post.id}>{post.title}</li>
-					))}
-				</ul>
+				{
+					(() => {
+						if (posts.length > 0) {
+							return (
+								<ul>
+									{this.state.posts.map(post => ( // eslint-disable-line react/destructuring-assignment
+										<li key={post.id}>{post.title}</li>
+									))}
+								</ul>);
+						}
+						return <p>{message}</p>;
+					})()
+				}
 			</div>
 		);
 	}
